b6464ab01cb367774f1d5164a036d8e6da33f6c0
John Andrey Galingan <johngalingan05@gmail.com>
2022-02-15 18:09:07 -0800

    Web layout and media queries has been added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./dist/index.html"],
+  content: ['./dist/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans'],
+      },
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+    },
   },
   plugins: [],
 };
